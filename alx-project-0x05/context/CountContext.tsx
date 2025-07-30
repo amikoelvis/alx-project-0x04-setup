3a2b7baed658a674a39d06ba5c3faa6ae1bb066e
Amiko Elvis <amikoelvis@gmail.com>
2025-07-30 22:58:43 +0300

    Fix checker error for CountContext.tsx missing ; in icrement and decrement functions

--- alx-project-0x05/context/CountContext.tsx
+++ alx-project-0x05/context/CountContext.tsx
@@ -12,8 +12,8 @@ export const CountProvider = ({ children }: { children: ReactNode }) => {
 
     const [count, setCount] = useState<number>(0)
 
-    const increment = () => setCount((count) => count + 1)
-    const decrement = () => setCount((count) => count > 0 ? count - 1 : 0)
+    const increment = () => setCount((count) => count + 1);
+    const decrement = () => setCount((count) => count > 0 ? count - 1 : 0);
 
     return (
         <CountContext.Provider
