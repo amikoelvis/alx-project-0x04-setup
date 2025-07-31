7d85d020179b71cb22d376a21e849b43836b09b2
Amiko Elvis <amikoelvis@gmail.com>
2025-07-31 09:58:15 +0300

    Include a Redux for global state management

--- alx-project-0x06/pages/counter-app.tsx
+++ alx-project-0x06/pages/counter-app.tsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
+import { AppDispatch, decrement, increment, RootState, useAppDispatch } from "@/store/store";
+import { useSelector } from "react-redux";
 
 const CounterApp: React.FC = () => {
-    const [count, setCount] = useState(0);
+    
+    const count = useSelector((state: RootState) => state.counter.value)
+    const dispatch: AppDispatch = useAppDispatch()
 
-    const increment = () => {
-        setCount(count + 1);
-    };
-
-    const decrement = () => {
-        setCount(count > 0 ? count - 1 : 0);
-    }
     return (
         <div className="min-h-screen bg-gradient-to-r from-yellow-400 to-pink-500 flex flex-col justify-center items-center text-white">
             {/* Title */}
@@ -28,13 +24,13 @@ const CounterApp: React.FC = () => {
             {/* Buttons */}
             <div className="flex space-x-4">
                 <button 
-                onClick={increment}
+                onClick={() => dispatch(increment())}
                 className="bg-green-500 hover:bg-green-600 text-white font-semibold py-3 px-8 rounded-full text-lg transition duration-300 shadow-lg transform hover:scale-105"
                 >
                     Increment 🚀
                 </button>
                 <button
-                onClick={decrement}
+                onClick={() => dispatch(decrement())}
                 className="bg-red-500 hover:bg-red-600 text-white font-semibold py-3 px-8 rounded-full text-lg transition duration-300 shadow-lg transform hover:scale-105"
                 >
                     Decrement 👎
