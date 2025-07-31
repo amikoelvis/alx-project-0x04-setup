9594513ba98dec03ea67696bdc17613ff2fc47bf
Amiko Elvis <amikoelvis@gmail.com>
2025-07-31 08:58:12 +0300

    Innitial commit

--- alx-project-0x06/pages/index.tsx
+++ alx-project-0x06/pages/index.tsx
@@ -21,11 +21,7 @@ export default function Home() {
 
       {/* Navigation Options */}
       <div className="flex gap-6">
-        <Button 
-        action={() => routeToNextPage({ pageRoute: '/generate-text-ai' })} 
-        buttonLabel="Generate Text" 
-        buttonBackgroundColor="blue" 
-        />
+        <Button action={() => routeToNextPage({ pageRoute: '/generate-text-ai' })} buttonLabel="Generate Text" buttonBackgroundColor="blue" />
         <Button action={() => routeToNextPage({ pageRoute: '/text-to-image'})} buttonLabel="Text to Image" buttonBackgroundColor="green" />
         <Button action={() => routeToNextPage({ pageRoute: '/counter-app'})} buttonLabel="Contact us" buttonBackgroundColor="orange" />
       </div>
